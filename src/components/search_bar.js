9ce65e81383e22fa7fa24115ee59b1e9de8a98e7
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-17 13:53:23 -0400

    add constructor to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
-const SearchBar = () => {
-    return <input / >
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            term: ''
+        }
+    }
+
+    render() {
+        return <input type="text" onChange={this.onInputChange} />
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value)
+    }
 }
 
 export default SearchBar
