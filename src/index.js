8ff7f6889cc87b9f2ff6a506fb53e75918114887
Ryan Magoon <mr.magoon5@gmail.com>
2017-05-17 02:18:34 -0400

    added search bar and eslint config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const CLIENT_ID = '316687114127-cgtb9vm2ah9p8u601en9n7un2dlm18a7.apps.googleusercontent.com'
+const API_KEY = 'qTnYQPmyQOYwtcePmw19sMCp'
+
 // Create a new component. This component should produce some HTML
 class App extends React.Component  {
-  render() {
-    return <div>Hi!</div>
-  }
+    render() {
+        return (
+            <div>
+              <SearchBar />
+            </div>
+        )
+    }
 }
 
 // Take this component's generated HTML and put it on the page
